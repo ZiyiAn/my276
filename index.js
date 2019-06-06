7301c0e37de054e68455ea72585a2d9a9ac07ad8
Ziyi An <ziyia@sfu.ca>
2019-06-06 16:54:19 -0700

    local database test

--- index.js
+++ index.js
@@ -4,7 +4,12 @@ const PORT = process.env.PORT || 5000
 const { Pool } = require('pg');
 const pool = new Pool({
   connectionString: process.env.DATABASE_URL,
-  ssl: true
+  //ssl: true,
+  port: 5432,
+  user: 'postgres',
+  password: 'ziyian',
+  host: 'localhost',
+  database: 'postgres'
 });
 
 express()
