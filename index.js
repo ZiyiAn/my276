4f84878f172819915cb385717e9db16f2d5658a0
Ziyi An <ziyia@sfu.ca>
2019-06-11 14:20:09 -0700

    table name changed

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const path = require('path')
 const PORT = process.env.PORT || 5000
 const { Pool } = require('pg');
 const pool = new Pool({
-  connectionString: process.env.DATABASE_URL,
+  connectionString: process.env.DATABASE_URL
   //ssl: true,
 
 });
