41af8025d7dcbade30352864c161d3ef45d42757
Ziyi An <ziyia@sfu.ca>
2019-06-14 18:39:04 -0700

    6.13

--- public/stuDB.js
+++ public/stuDB.js
@@ -4,10 +4,8 @@
 
 function validInsert(){
 	var message1 = document.getElementById("message1");
-	// var message2 = document.getElementById("message2");
 	var data1 = document.getElementById("form1");
 	var name = data1[0].value
-	// var gender = data1[1].checked?"M":data1[2].checked?"F":"O"
 	var weight = parseFloat(data1[4].value)
 	var height = parseFloat(data1[5].value)
 	var color = data1[6].value
@@ -19,16 +17,16 @@ function validInsert(){
 	if (name.length==0)
 		message += " name "
 
-	if (isNaN(weight)||weight<=0)
+	if (isNaN(weight)||weight<=0||weight.length==0)
 		message += " weight "
 
-	if (isNaN(height)||height<=0)
+	if (isNaN(height)||height<=0||height.length==0)
 		message += " height "
 
 	if (color.length==0)
 		message += " color "
 
-	if (isNaN(gpa)||gpa<0)
+	if (isNaN(gpa)||gpa<0||gpa.length==0)
 		message += " gpa "
 
 	if (message!=""){
@@ -46,33 +44,14 @@ function validInsert(){
 
 function validSearch(){
 	var message1 = document.getElementById("message2");
-	// var message2 = document.getElementById("message2");
 	var data1 = document.getElementById("form2");
 	var name = data1[0].value
-	// var gender = data1[1].checked?"M":data1[2].checked?"F":"O"
-	// var weight = parseFloat(data1[4].value)
-	// var height = parseFloat(data1[5].value)
-	// var color = data1[6].value
-	// var gpa = parseFloat(data1[7].value)
-
 
 	message = ""
 
 	if (name.length==0)
 		message += " name "
-
-	// if (isNaN(weight)||weight<=0)
-	// 	message += " weight "
-
-	// if (isNaN(height)||height<=0)
-	// 	message += " height "
-
-	// if (color.length==0)
-	// 	message += " color "
-
-	// if (isNaN(gpa)||gpa<0)
-	// 	message += " gpa "
-
+\
 	if (message!=""){
 		message1.innerHTML = "<b>"+ message + "invalid</b>"
 		message1.style.color = "#ff0000"
